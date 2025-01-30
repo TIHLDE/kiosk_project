d858962dc32bbf531ed8067fe4e6e5886700c75e
Mathias Hølestøl <mathiash@uia.no>
2025-01-30 19:57:55 +0100

    fixed trash ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,99 +4,120 @@ import { useEffect, useState } from "react";
 import { PurchaseStatistics } from "./api/zettle/zettle_data";
 
 export default function Home() {
-  const [data, setData] = useState<PurchaseStatistics | undefined>(undefined);
+    const [data, setData] = useState<PurchaseStatistics | undefined>(undefined);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      console.log("Fetching data");
-      try {
-        const response = await fetch("/api/zettle");
-        const newData = await response.json();
-        setData(newData);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+    useEffect(() => {
+        const fetchData = async () => {
+            console.log("Fetching data");
+            try {
+                const response = await fetch("/api/zettle");
+                const newData = await response.json();
+                setData(newData);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
 
-    fetchData();
+        fetchData();
 
-    const intervalId = setInterval(fetchData, 20000);
+        const intervalId = setInterval(fetchData, 20000);
 
-    return () => clearInterval(intervalId);
-  }, []);
+        return () => clearInterval(intervalId);
+    }, []);
 
-  return (
-    <div className="bg-black w-full h-screen text-white flex flex-col items-center justify-center">
-      <h1 className="text-2xl font-bold">
-        Gjennomsnittlig betalingsbeløp siste {data?.numberOfPurchases}{" "}
-        betalinger ({formatDate(data?.startDate || new Date())} -{" "}
-        {formatDate(data?.endDate || new Date())})
-      </h1>
-      {data ? (
-        <div>
-          <p className="text-4xl pt-10">{data.averagePayment},-</p>
+    return (
+        <div
+            className="bg-gradient-to-bl from-gray-100 to-gray-300 w-full h-screen text-white grid grid-cols-2 grid-rows-2 gap-4 p-4 justify-center">
+            <div
+                className="bg-gray-100 flex-box p-12 min-w-60 min-h-30 text-black flex flex-col items-center justify-center rounded-xl">
+                {data ? (
+                    <div
+                        className="w-40 h-40 flex justify-center items-center rounded-full border-2 border-sky-700 shadow-md">
+                        <p className="text-3xl font-bold">{data.averagePayment},-</p>
+                    </div>
+                ) : (
+                    <div className="text-4xl pt-10">
+                        <p>Loading...</p>
+                    </div>
+                )}
+                <h1 className="text-2xl font-bold pt-10 text-center">
+                    Gjennomsnittlig betalingsbeløp siste {data?.numberOfPurchases}{" "}
+                    betalinger ({formatDate(data?.startDate || new Date())} -{" "}
+                    {formatDate(data?.endDate || new Date())})
+                </h1>
+            </div>
+            <div
+                className="bg-gray-100 flex-box p-12 text-black flex flex-col justify-center rounded-xl space-y-4">
+                <h1 className="text-2xl font-bold">
+                    Mest solgte produkter siste {data?.numberOfPurchases} betalinger
+                </h1>
+                {data ? (
+                    <div className="grid grid-cols-3 gap-3">
+                        {data.mostSoldProductsByItems.slice(0, 9).map((product, index) => (
+                            <div className="space-y-1 border-2 border-sky-700 rounded-lg p-2" key={index}>
+                                <p className="text-center font-semibold text-xl">
+                                    {product.quantity}
+                                </p>
+                                <p className="text-center">
+                                    {product.name.slice(0, 20)}
+                                </p>
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <div>
+                        <p>Loading...</p>
+                    </div>
+                )}
+            </div>
+            <div
+                className="bg-gray-100 flex-box p-12 text-black flex flex-col items-center justify-center rounded-xl space-y-4">
+                <h1 className="text-2xl font-bold">
+                    Mest inntektsgivende produkter siste {data?.numberOfPurchases}{" "}
+                    betalinger
+                </h1>
+                {data ? (
+                        <div className="grid grid-cols-3 gap-3">
+                            {data.mostSoldProductsByRevenue.slice(0, 9).map((product, index) => (
+                                <div className="space-y-1 border-2 border-sky-700 rounded-lg p-2" key={index}>
+                                    <p className="text-center font-semibold text-xl">
+                                        {product.amount},-
+                                    </p>
+                                    <p className="text-center">
+                                        {product.name.slice(0, 20)}
+                                    </p>
+                                </div>
+                            ))}
+                        </div>
+                ) : (
+                    <div>
+                        <p>Loading...</p>
+                    </div>
+                )}
+            </div>
+            <div
+                className="bg-gray-100 flex-box p-12 min-w-60 min-h-30 text-black flex flex-col items-center justify-center rounded-xl">
+                {data ? (
+                    <div
+                        className="w-40 h-40 flex justify-center items-center rounded-full border-2 border-sky-700 shadow-md">
+                        <p className="text-3xl font-bold"> {data.numberOfEnergyDrinksSold}</p>
+                    </div>
+                ) : (
+                    <div>
+                        <p>Loading...</p>
+                    </div>
+                )}
+                <h1 className="text-2xl font-bold pt-10">
+                    Antall energidrikker solgt siste {data?.numberOfPurchases} betalinger
+                </h1>
+            </div>
         </div>
-      ) : (
-        <div className="text-4xl pt-10">
-          <p>Loading...</p>
-        </div>
-      )}
-      <h1 className="text-2xl font-bold pt-10">
-        Mest solgte produkter siste {data?.numberOfPurchases} betalinger
-      </h1>
-      {data ? (
-        <div>
-          <ul>
-            {data.mostSoldProductsByItems.slice(0, 10).map((product) => (
-              <li key={product.name}>
-                {product.name}: {product.quantity}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ) : (
-        <div>
-          <p>Loading...</p>
-        </div>
-      )}
-      <h1 className="text-2xl font-bold pt-10">
-        Mest inntektsgivende produkter siste {data?.numberOfPurchases}{" "}
-        betalinger
-      </h1>
-      {data ? (
-        <div>
-          <ul>
-            {data.mostSoldProductsByRevenue.slice(0, 10).map((product) => (
-              <li key={product.name}>
-                {product.name}: {product.amount},-
-              </li>
-            ))}
-          </ul>
-        </div>
-      ) : (
-        <div>
-          <p>Loading...</p>
-        </div>
-      )}
-      <h1 className="text-2xl font-bold pt-10">
-        Antall energidrikker solgt siste {data?.numberOfPurchases} betalinger
-      </h1>
-      {data ? (
-        <div>
-          <p>{data.numberOfEnergyDrinksSold}</p>
-        </div>
-      ) : (
-        <div>
-          <p>Loading...</p>
-        </div>
-      )}
-    </div>
-  );
+    );
 }
 
 function formatDate(date: Date): string {
-  const dateObject = new Date(date);
-  return `${dateObject.getDate().toString()}.${(
-    dateObject.getMonth() + 1
-  ).toString()}.${dateObject.getFullYear().toString()}`;
-}
+    const dateObject = new Date(date);
+    return `${dateObject.getDate().toString()}.${(
+            dateObject.getMonth() + 1
+            ).toString()}.${dateObject.getFullYear().toString()}`;
+        }
