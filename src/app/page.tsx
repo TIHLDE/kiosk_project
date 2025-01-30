dc1be9e1cc49800d2a8dd6059f708b8e8658c712
Mads Nylund <madsnyl@gmail.com>
2025-01-30 20:27:30 +0100

    added relaod component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import ReloadComponent from "./reload-component";
 import { getPurchaseStats } from "./server/zettle";
 
 export default async function Home() {
@@ -13,90 +14,93 @@ export default async function Home() {
   }
 
   return (
-    <div className="bg-gradient-to-bl from-gray-100 to-gray-300 w-full h-screen text-white grid grid-cols-2 grid-rows-2 gap-4 p-4 justify-center">
-      <div className="bg-gray-100 flex-box p-12 min-w-60 min-h-30 text-black flex flex-col items-center justify-center rounded-xl">
-        {data ? (
-          <div className="w-40 h-40 flex justify-center items-center rounded-full border-2 border-sky-700 shadow-md">
-            <p className="text-3xl font-bold">{data.averagePayment},-</p>
-          </div>
-        ) : (
-          <div className="text-4xl pt-10">
-            <p>Loading...</p>
-          </div>
-        )}
-        <h1 className="text-2xl font-bold pt-10 text-center">
-          Gjennomsnittlig betalingsbeløp siste {data?.numberOfPurchases}{" "}
-          betalinger ({formatDate(data?.startDate || new Date())} -{" "}
-          {formatDate(data?.endDate || new Date())})
-        </h1>
-      </div>
-      <div className="bg-gray-100 flex-box p-12 text-black flex flex-col justify-center rounded-xl space-y-4">
-        <h1 className="text-2xl font-bold">
-          Mest solgte produkter siste {data?.numberOfPurchases} betalinger
-        </h1>
-        {data ? (
-          <div className="grid grid-cols-3 gap-3">
-            {data.mostSoldProductsByItems.slice(0, 9).map((product, index) => (
-              <div
-                className="space-y-1 border-2 border-sky-700 rounded-lg p-2"
-                key={index}
-              >
-                <p className="text-center font-semibold text-xl">
-                  {product.quantity}
-                </p>
-                <p className="text-center">{product.name.slice(0, 20)}</p>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <div>
-            <p>Loading...</p>
-          </div>
-        )}
-      </div>
-      <div className="bg-gray-100 flex-box p-12 text-black flex flex-col items-center justify-center rounded-xl space-y-4">
-        <h1 className="text-2xl font-bold">
-          Mest inntektsgivende produkter siste {data?.numberOfPurchases}{" "}
-          betalinger
-        </h1>
-        {data ? (
-          <div className="grid grid-cols-3 gap-3">
-            {data.mostSoldProductsByRevenue
-              .slice(0, 9)
-              .map((product, index) => (
+    <div>
+      <ReloadComponent />
+      <div className="bg-gradient-to-bl from-gray-100 to-gray-300 w-full h-screen text-white grid grid-cols-2 grid-rows-2 gap-4 p-4 justify-center">
+        <div className="bg-gray-100 flex-box p-12 min-w-60 min-h-30 text-black flex flex-col items-center justify-center rounded-xl">
+          {data ? (
+            <div className="w-40 h-40 flex justify-center items-center rounded-full border-2 border-sky-700 shadow-md">
+              <p className="text-3xl font-bold">{data.averagePayment},-</p>
+            </div>
+          ) : (
+            <div className="text-4xl pt-10">
+              <p>Loading...</p>
+            </div>
+          )}
+          <h1 className="text-2xl font-bold pt-10 text-center">
+            Gjennomsnittlig betalingsbeløp siste {data?.numberOfPurchases}{" "}
+            betalinger ({formatDate(data?.startDate || new Date())} -{" "}
+            {formatDate(data?.endDate || new Date())})
+          </h1>
+        </div>
+        <div className="bg-gray-100 flex-box p-12 text-black flex flex-col justify-center rounded-xl space-y-4">
+          <h1 className="text-2xl font-bold">
+            Mest solgte produkter siste {data?.numberOfPurchases} betalinger
+          </h1>
+          {data ? (
+            <div className="grid grid-cols-3 gap-3">
+              {data.mostSoldProductsByItems.slice(0, 9).map((product, index) => (
                 <div
                   className="space-y-1 border-2 border-sky-700 rounded-lg p-2"
                   key={index}
                 >
                   <p className="text-center font-semibold text-xl">
-                    {product.amount},-
+                    {product.quantity}
                   </p>
                   <p className="text-center">{product.name.slice(0, 20)}</p>
                 </div>
               ))}
-          </div>
-        ) : (
-          <div>
-            <p>Loading...</p>
-          </div>
-        )}
-      </div>
-      <div className="bg-gray-100 flex-box p-12 min-w-60 min-h-30 text-black flex flex-col items-center justify-center rounded-xl">
-        {data ? (
-          <div className="w-40 h-40 flex justify-center items-center rounded-full border-2 border-sky-700 shadow-md">
-            <p className="text-3xl font-bold">
-              {" "}
-              {data.numberOfEnergyDrinksSold}
-            </p>
-          </div>
-        ) : (
-          <div>
-            <p>Loading...</p>
-          </div>
-        )}
-        <h1 className="text-2xl font-bold pt-10">
-          Antall energidrikker solgt siste {data?.numberOfPurchases} betalinger
-        </h1>
+            </div>
+          ) : (
+            <div>
+              <p>Loading...</p>
+            </div>
+          )}
+        </div>
+        <div className="bg-gray-100 flex-box p-12 text-black flex flex-col items-center justify-center rounded-xl space-y-4">
+          <h1 className="text-2xl font-bold">
+            Mest inntektsgivende produkter siste {data?.numberOfPurchases}{" "}
+            betalinger
+          </h1>
+          {data ? (
+            <div className="grid grid-cols-3 gap-3">
+              {data.mostSoldProductsByRevenue
+                .slice(0, 9)
+                .map((product, index) => (
+                  <div
+                    className="space-y-1 border-2 border-sky-700 rounded-lg p-2"
+                    key={index}
+                  >
+                    <p className="text-center font-semibold text-xl">
+                      {product.amount},-
+                    </p>
+                    <p className="text-center">{product.name.slice(0, 20)}</p>
+                  </div>
+                ))}
+            </div>
+          ) : (
+            <div>
+              <p>Loading...</p>
+            </div>
+          )}
+        </div>
+        <div className="bg-gray-100 flex-box p-12 min-w-60 min-h-30 text-black flex flex-col items-center justify-center rounded-xl">
+          {data ? (
+            <div className="w-40 h-40 flex justify-center items-center rounded-full border-2 border-sky-700 shadow-md">
+              <p className="text-3xl font-bold">
+                {" "}
+                {data.numberOfEnergyDrinksSold}
+              </p>
+            </div>
+          ) : (
+            <div>
+              <p>Loading...</p>
+            </div>
+          )}
+          <h1 className="text-2xl font-bold pt-10">
+            Antall energidrikker solgt siste {data?.numberOfPurchases} betalinger
+          </h1>
+        </div>
       </div>
     </div>
   );
