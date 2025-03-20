1f5b9fb155ce4e8a7d9bfd9126ae8e3150a9ffff
Mateo Souto <mateo.souto@outlook.com>
2025-03-20 19:43:23 +0100

    test: more testing

--- src/components/display-cards/test.tsx
+++ src/components/display-cards/test.tsx
@@ -5,7 +5,7 @@ import CardWrapper from "./wrapper";
 export default function TestCard() {
     return (
         <CardWrapper className="flex items-center justify-center">
-            <div>Her tester vi mann</div>
+            <div>Her tester vin</div>
         </CardWrapper>
     );
 };
