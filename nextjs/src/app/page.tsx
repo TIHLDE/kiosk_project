379acb3ad1c60c8ceb5cefebe99046b3e0797ab5
Mateo Souto <mateo.souto@outlook.com>
2025-05-02 19:28:40 +0200

    fix: changes so that dev works

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
   useEffect(() => {
     const fetchPurchase = async () => {
       try {
-        const response = await fetch('https://kiosken.tihlde.org/api/zettle/purchases', {
+        const response = await fetch(`https://${process.env.NEXT_PUBLIC_URL}/api/zettle/purchases`, {
           method: 'GET',
         });
 
