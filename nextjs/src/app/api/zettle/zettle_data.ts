dec1b690db6769e5aad43275ce4d60fd15a97714
Mateo Souto <mateo.souto@outlook.com>
2025-05-03 19:18:40 +0200

    fix: stupid typo

--- nextjs/src/app/api/zettle/zettle_data.ts
+++ nextjs/src/app/api/zettle/zettle_data.ts
@@ -64,7 +64,7 @@ export function mostBoughtProducts(purchases: Purchase[]): ProductCount[] {
 //   );
 // }
 
-export function numberOfEnergyDrinksBought(purchases: Purchase[]): number {
+export function numberOfEnergyDrinksBougt(purchases: Purchase[]): number {
   return purchases.reduce((acc, purchase) => {
     return (
       acc +
