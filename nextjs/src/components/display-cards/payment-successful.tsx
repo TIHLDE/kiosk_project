b3a2bcd82dfff8743a03192e4339898983d028fd
mathole01 <mathias@dhcp-10-22-16-127.wlan.ntnu.no>
2025-05-15 17:38:28 +0200

    fix black background

--- nextjs/src/components/display-cards/payment-successful.tsx
+++ nextjs/src/components/display-cards/payment-successful.tsx
@@ -7,7 +7,7 @@ export default function PaymentSuccessful() {
       <div
         className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gray-50"
       >
-        <main className="flex flex-col items-center gap-8 row-start-2 bg-white p-12 rounded-lg shadow-lg sm:items-start">
+          <main className="flex flex-col items-center gap-8 row-start-2 bg-transparent  p-12 rounded-lg sm:items-start">
           <h1 className="text-4xl font-bold text-center sm:text-5xl sm:text-middle pt-4">
             Betaling vellykket
           </h1>
