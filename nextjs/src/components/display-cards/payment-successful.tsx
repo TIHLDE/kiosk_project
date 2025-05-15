8379007b6abb675c33355229e3886e29f017deb9
Mateo Souto <mateo.souto@outlook.com>
2025-05-15 18:35:48 +0200

    change color to black

--- nextjs/src/components/display-cards/payment-successful.tsx
+++ nextjs/src/components/display-cards/payment-successful.tsx
@@ -8,7 +8,7 @@ export default function PaymentSuccessful() {
         className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gray-50"
       >
           <main className="flex flex-col items-center gap-8 row-start-2 bg-transparent  p-12 rounded-lg sm:items-start">
-          <h1 className="text-4xl font-bold text-center sm:text-5xl sm:text-middle pt-4">
+          <h1 className="text-4xl font-bold text-center sm:text-5xl sm:text-middle pt-4 text-black">
             Betaling vellykket
           </h1>
           <div className="flex justify-center items-center h-full w-full">
